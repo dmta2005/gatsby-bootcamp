22c86f527c09da61dd89407fd92ae7aa09806d15
jwunderly <jwunderly@supremecourt.gov>
2020-01-31 11:07:51 -0500

    Final Gatsby site

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+const AboutPage = () => {
+    return (
+        <Layout>
+             <Head title="About" />
+            <h1>About Me</h1>
+            <p>I currently learning Gatbsy code</p>
+            <p><Link to="/contact">Want to work with me? Reach out.</Link></p>
+     </Layout>   
+    )
+}
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+
+const IndexPage = () => {
+    return (
+       
+            <Layout>
+                <Head title="Home" />
+                <h1>Hello.</h1>
+                <h2>I'm Ta Fularon, a full-stack developer living in beautiful Virginia</h2>
+                <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+            </Layout>
+           
+        
+                   
+    
+    )
+}
+export default IndexPage
